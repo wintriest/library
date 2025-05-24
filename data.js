cdf12643afd241c05e41c8ddf559f565a54c53f0
wintriest <wintriests@protonmail.com>
2025-05-25 05:47:45 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,6 @@
+export const interiors = [
+    { id: 1, name: "Letters beyond Time" },
+    { id: 2, name: "Ephemeral Suite" },
+    { id: 3, name: "A Great Secret" },
+    { id: 4, name: "An Unslightly Past" },
+];
---
+++ script.js
@@ -0,0 +1,109 @@
+import { interiors } from "./data.js";
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cursor = document.querySelector(".cursor");
+    const gallery = document.querySelector(".gallery");
+    const numberOfItems = 4;
+    const radius = 1100;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const angleIncrement = (2 * Math.PI) / numberOfItems;
+
+    for (let i = 0; i < numberOfItems; i++) {
+        const item = document.createElement("div");
+        item.className = "item";
+        const a = document.createElement("a");
+        const count = document.createElement("span");
+        a.textContent = interiors[i].name;
+        const titleSlug = interiors[i].name.toLowerCase().replace(/\s+/g, '-');
+        a.href = `./novels/${titleSlug}.html`;
+        a.classList.add("story-link");
+        count.textContent = `(${Math.floor(Math.random() * 4) + 1}]`;
+        item.appendChild(a);
+        // p.appendChild(count); #removed visual for assigned #.
+        gallery.appendChild(item);
+
+        const angle = i * angleIncrement;
+        const x = centerX + radius * Math.cos(angle);
+        const y = centerY + radius * Math.sin(angle);
+        const rotation = (angle * 180) / Math.PI;
+
+        gsap.set(item, {
+            x: x + "px",
+            y: y + "px",
+            rotation: rotation, 
+        });
+
+        /* item.addEventListener("mouseover", function () {
+            const imgSrc = `./assets/img${i + 1}.jpg`;
+            const img = document.createElement("img");
+            img.src = imgSrc;
+            img.style.clipPath = "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)";
+            cursor.appendChild(img);
+
+            gsap.to(img, {
+                clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+                durating: 1,
+                ease: "power3.out",
+            });
+        });
+
+        item.addEventListener("mouseout", function () {
+            const imgs = cursor.getElementsByTagName("img");
+            if(imgs.length) {
+                const lastImg = imgs[imgs.length - 1];
+                Array.from(imgs).forEach((img, index) => {
+                    if (img !== lastImg) {
+                        gsap.to(img, {
+                            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+                            durating: 1,
+                            ease: "power3.out",
+                            onComplete: () => {
+                                setTimeout(() => {
+                                    img.remove();
+                                }, 1000);
+                            },
+                        });
+                    }
+                });
+
+                gsap.to(lastImg, {
+                    clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+                    durating: 1,
+                    ease: "power3.out",
+                    delay: 0.25,
+                })
+            } */
+
+            /* function updatePosition() {
+                const scrollAmount = window.scrollY * 0.00085;
+                document.querySelectorAll(".item").forEach(function (item, index) {
+                    const angle = index * angleIncrement + scrollAmount;
+                    const x = centerX + radius * Math.cos(angle);
+                    const y = centerY + radius * Math.sin(angle);
+                    const rotation = (angle * 180) / Math.PI;
+
+                    gsap.to(item, {
+                        duration: 0.05,
+                        x: x + "px",
+                        y: y + "px",
+                        rotation: rotation, 
+                        ease: "elastic.out(1, 0.3)",
+                    });
+                });
+            }
+
+            updatePosition();
+            document.addEventListener("scroll", updatePosition);
+
+            document.addEventListener("mousemove", function (e) {
+                gsap.to(cursor, {
+                    x: e.clientX - 150,
+                    y: e.clientY - 200,
+                    duration: 1,
+                    ease: "power3.out",
+                });
+            }); */
+        // });
+    }
+});
